9cb566dc6e00cd2dfaee383f44a73fdf224e569d
Yuumi Yoshida <yy@ey-office.com>
2021-08-15 15:50:41 +0900

    Improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 docker-compose down
 
 git pull origin master
+chmod 777 pgadmin
 
 docker-compose -f docker-compose.yml -f docker-production.yml build
 docker-compose -f docker-compose.yml -f docker-production.yml up -d
